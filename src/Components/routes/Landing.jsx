58f42a9899762d146261c307c82db38a989120eb
Nilesh-pandey12 <142581735+Nilesh-palanam@users.noreply.github.com>
2024-04-02 16:47:06 +0530

    updated today the nav bar

--- src/Components/routes/Landing.jsx
+++ src/Components/routes/Landing.jsx
@@ -6,16 +6,16 @@ import {
     
   } from "react-router-dom";
 import Header from '../Header/Header';
-import Body from '../Body';
-import Footer from '../Footer';
-import Contacts from '../contact'; 
-import AboutUs from '../aboutUs'; 
+import Body from '../Body/Body';
+import Footer from '../Footer/Footer';
+import Contacts from '../Contact/contact'; 
+import AboutUs from '../Aboutus/aboutUs'; 
 
 const Landing = () => {
     return (
         <Router>
             <Routes>
-                <Route path='/' element={<><Header/> <Body/> <Footer/></>}/>
+                <Route path='/home' element={<><Header/> <Body/> <Footer/></>}/>
             </Routes>
         </Router>
     );
